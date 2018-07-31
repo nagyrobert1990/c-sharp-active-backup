f2d9a880e03e8adac271b1febc82f94018380bb8
nagyrobert1990 <nagy.robert1990@gmail.com>
2018-07-31 11:59:07 +0200

    directory size checker with progress bar is ready

--- ActiveBackup/ActiveBackup.Designer.cs
+++ ActiveBackup/ActiveBackup.Designer.cs
@@ -28,20 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblSelectedFldr = new System.Windows.Forms.Label();
+            this.lblSelectedBackup = new System.Windows.Forms.Label();
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.txtBackup = new System.Windows.Forms.TextBox();
+            this.progBarFolder = new System.Windows.Forms.ProgressBar();
+            this.lblProgress = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lblSelectedFldr
+            // 
+            this.lblSelectedFldr.AutoSize = true;
+            this.lblSelectedFldr.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.lblSelectedFldr.Location = new System.Drawing.Point(12, 34);
+            this.lblSelectedFldr.Name = "lblSelectedFldr";
+            this.lblSelectedFldr.Size = new System.Drawing.Size(81, 13);
+            this.lblSelectedFldr.TabIndex = 0;
+            this.lblSelectedFldr.Text = "Selected Folder";
+            // 
+            // lblSelectedBackup
+            // 
+            this.lblSelectedBackup.AutoSize = true;
+            this.lblSelectedBackup.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.lblSelectedBackup.Location = new System.Drawing.Point(386, 34);
+            this.lblSelectedBackup.Name = "lblSelectedBackup";
+            this.lblSelectedBackup.Size = new System.Drawing.Size(89, 13);
+            this.lblSelectedBackup.TabIndex = 1;
+            this.lblSelectedBackup.Text = "Selected Backup";
+            // 
+            // txtFolder
+            // 
+            this.txtFolder.Location = new System.Drawing.Point(15, 51);
+            this.txtFolder.Name = "txtFolder";
+            this.txtFolder.ReadOnly = true;
+            this.txtFolder.Size = new System.Drawing.Size(300, 20);
+            this.txtFolder.TabIndex = 2;
+            this.txtFolder.DoubleClick += new System.EventHandler(this.TxtFolder_DoubleClick);
+            // 
+            // txtBackup
+            // 
+            this.txtBackup.Location = new System.Drawing.Point(389, 51);
+            this.txtBackup.Name = "txtBackup";
+            this.txtBackup.ReadOnly = true;
+            this.txtBackup.Size = new System.Drawing.Size(300, 20);
+            this.txtBackup.TabIndex = 3;
+            this.txtBackup.DoubleClick += new System.EventHandler(this.TxtBackup_DoubleClick);
+            // 
+            // progBarFolder
+            // 
+            this.progBarFolder.Location = new System.Drawing.Point(15, 77);
+            this.progBarFolder.Maximum = 90;
+            this.progBarFolder.Minimum = 30;
+            this.progBarFolder.Name = "progBarFolder";
+            this.progBarFolder.Size = new System.Drawing.Size(300, 23);
+            this.progBarFolder.TabIndex = 4;
+            this.progBarFolder.Value = 30;
+            // 
+            // lblProgress
+            // 
+            this.lblProgress.AutoSize = true;
+            this.lblProgress.Location = new System.Drawing.Point(135, 82);
+            this.lblProgress.Name = "lblProgress";
+            this.lblProgress.Size = new System.Drawing.Size(0, 13);
+            this.lblProgress.TabIndex = 5;
+            this.lblProgress.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // ActiveBackup
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(701, 312);
+            this.Controls.Add(this.lblProgress);
+            this.Controls.Add(this.progBarFolder);
+            this.Controls.Add(this.txtBackup);
+            this.Controls.Add(this.txtFolder);
+            this.Controls.Add(this.lblSelectedBackup);
+            this.Controls.Add(this.lblSelectedFldr);
             this.Name = "ActiveBackup";
             this.Text = "ActiveBackup";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ActiveBackup_FormClosing);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblSelectedFldr;
+        private System.Windows.Forms.Label lblSelectedBackup;
+        private System.Windows.Forms.TextBox txtFolder;
+        private System.Windows.Forms.TextBox txtBackup;
+        private System.Windows.Forms.ProgressBar progBarFolder;
+        private System.Windows.Forms.Label lblProgress;
     }
 }
 
--- ActiveBackup/ActiveBackup.cs
+++ ActiveBackup/ActiveBackup.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +14,101 @@ namespace ActiveBackup
 {
     public partial class ActiveBackup : Form
     {
+        static readonly int maxFolderSize = 90;
+        static readonly int minFolderSize = 30;
+
+        Thread th;
+
         public ActiveBackup()
         {
             InitializeComponent();
+        }
+
+        private void TxtFolder_DoubleClick(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog() { Description = "Select path" })
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    txtFolder.Text = fbd.SelectedPath;
+
+                    lblProgress.Text = "";
+                    progBarFolder.Value = 30;
+
+                    th = new Thread(() => InvokeMethod(fbd.SelectedPath));
+                    th.Start();
+                }
+            }
+        }
+
+        private void TxtBackup_DoubleClick(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog() { Description = "Select path" })
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    txtBackup.Text = fbd.SelectedPath;
+                }
+            }
+        }
+
+        private void InvokeMethod(string filePath)
+        {
+            while (true)
+            {
+                GiveSizeToProgBar(filePath);
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void GiveSizeToProgBar(string filePath)
+        {
+            long dirSize = GetDirectorySize(filePath);
+            if (IsSizeInRange(filePath) == 0)
+            {
+                lblProgress.Text = dirSize.ToString() + " kb";
+                progBarFolder.Value = (int)dirSize;
+            }
+        }
+
+        private int IsSizeInRange(string filePath)
+        {
+            int inRange = 0;
+            long dirSize = GetDirectorySize(filePath);
+
+            if (dirSize < maxFolderSize && dirSize > minFolderSize)
+            {
+                inRange = 0;
+            }
+            else if (dirSize >= maxFolderSize)
+            {
+                inRange = 1;
+            }
+            else if (dirSize <= minFolderSize)
+            {
+                inRange = -1;
+            }
+            
+            return inRange;
+        }
+
+        //kbytes
+        private long GetDirectorySize(string filePath)
+        {
+            var files = Directory.EnumerateFiles(filePath);
+
+            var currentSize = (from file in files let fileInfo = new FileInfo(file) select fileInfo.Length).Sum();
+
+            var directories = Directory.EnumerateDirectories(filePath);
+
+            var subDirSize = (from directory in directories select GetDirectorySize(directory)).Sum();
+
+            return (currentSize + subDirSize)/1024;
+        }
+
+        private void ActiveBackup_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            th.Abort();
         }
     }
 }
